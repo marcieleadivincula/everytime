bf8be1c4fdb1b0cf5a261de80a66664d9c885f74
Marciele <marcieleadivincula@gmail.com>
2019-07-23 18:53:36 -0300

    [JS][employee]Cadastro de funcionarios

--- src/js/employee.js
+++ src/js/employee.js
@@ -1,57 +1,103 @@
 const database = firebase.database();
-const signUpBtn = document.querySelector(".register-btn");
-const emailInput = document.querySelector("#signUpEmail");
-const passwordInput = document.querySelector("#signUpPassword");
+let signUpBtn = document.querySelector(".register-btn");
+let emailInput = document.querySelector(".signUpEmail");
+let passwordInput = document.querySelector(".signUpPassword");
+let profileInput = document.querySelector("#profileInput");
+let valueSelect = "";
+
+profileInput.addEventListener('change', function () {
+    valueSelect = profileInput.value;
+    return valueSelect;
+})
 
 
-window.onload = () => {
-    typing();
-    signUpBtn.addEventListener('click', signUp);
-}
-
-function manage(email, password) {
-    if (email.value !== '' && password.value !== '') {
-        signUpBtn.disabled = false;
-    } else {
-        signUpBtn.disabled = true;
-    }
-}
-
-function typing() {
-    let inputs = [emailInput, passwordInput];
-    inputs.forEach(function(el) {
-        el.addEventListener("keyup", function() {
-            manage(emailInput, passwordInput);
-        });
-    });
-}
-
-function signUp(e) {
-    e.preventDefault();
-    const email = emailInput.value;
-    const password = passwordInput.value;
-    createUser(email, password);
-}
-
-function createUser(email, password) {
+signUpBtn.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    email = emailInput.value;
+    password = passwordInput.value;
+    console.log(email, password, valueSelect)
     firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function(response) {
-            const userId = response.user.uid;
-            database.ref('users/' + userId).set({
-                email: email
-            });
-            createProfile(userId);
+        .then((res) => {
+            saveUserInfos(email, password, valueSelect, res.user.uid);
+            console.log('entrou');
+            // window.location = 'page.html';
         })
-        .catch(function(error) {
-            handleErrors(error)
+        .catch((error) => {
+            console.log(error.code);
+        })
+});
+
+let saveUserInfos = (email, password, select, uid) => {
+    database.ref('users/' + uid)
+        .set({
+            email: email,
+            pass: password,
+            profile: select
         });
 }
 
-function handleErrors(error) {
-    const errorMessage = error.message;
-    alert(errorMessage);
-}
+// window.onload = () => {
+//     typing();
+//     signUpBtn.addEventListener('click', signUp);
+// }
 
-function createProfile(userId) {
-    window.location = `employee.html?id=${userId}`;
-}
+// function manage(email, password) {
+//     if (email.value !== '' && password.value !== '') {
+//         signUpBtn.disabled = false;
+//     } else {
+//         signUpBtn.disabled = true;
+//     }
+// }
+
+// function typing() {
+//     let inputs = [emailInput, passwordInput];
+//     inputs.forEach(function (el) {
+//         el.addEventListener("keyup", function () {
+//             manage(emailInput, passwordInput);
+//         });
+//     });
+// }
+
+// // function createUser() {
+// //     const email = emailInput.value;
+// //     console.log(email)
+// //     const password = passwordInput.value;
+// //     console.log(password)
+// //     database.collection('users').set({
+// //         email: email
+// //     });
+// //     createProfile(userId);
+// // }
+
+// function signUp(e) {
+//     e.preventDefault();
+//     const email = emailInput.value;
+//     const password = passwordInput.value;
+//     createUser(email, password);
+// }
+
+// function createUser(email, password) {
+//     firebase.auth().createUserWithEmailAndPassword(email, password)
+//         .then((response) => {
+//             const userId = response.user.uid;
+//             database.collection('users' + userId).set({
+//                 username: name,
+//                 email: email,
+//                 // profile_picture: imageUrl
+//             })
+//             createProfile(userId);
+//         })
+//         .catch((error) => {
+//             handleErrors(error)
+//         });
+// }
+
+
+// function handleErrors(error) {
+//     const errorMessage = error.message;
+//     alert(errorMessage);
+// }
+
+// function createProfile(userId) {
+//     window.location = `index.html?id=${userId}`;
+// }
