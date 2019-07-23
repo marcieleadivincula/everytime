6680383e86b39be581621a6b21a8e99db653a89c
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-23 18:58:11 -0300

    [HUII-inOut]actual version getWorkedHours, still conflict with module.exports

--- src/ClockInOut.js
+++ src/ClockInOut.js
@@ -1,16 +1,14 @@
-
-// const moment = require('moment');
 const moment = require("moment-business-time");
 
-function getMoment () {
-  const checkIn = moment();
-  console.log(checkIn)
-}
+// Resolver error module
+// function getMoment () {
+//   const checkIn = moment();
+//   console.log(checkIn)
+// }
+// let clickIn = document.getElementById('check-in').addEventListener('click', getMoment())
+// console.log(clickIn);
 
 const checkIn = moment();
-
-console.log('teste check-in' + document.getElementById('check-in').addEventListener('click', getMoment()));
-
 const date = new Date;
 const min = date.getMinutes();
 const checkOut = checkIn.subtractWorkingTime(7, 'hours', min, 'minutes');
@@ -20,5 +18,4 @@ function getWorkedHours(checkIn, checkOut) {
    return checkOut - checkIn === 0 ? 'Horas ok' : 'Horas pend'
 
 }
-
 console.log(getWorkedHours(checkIn, checkOut));
