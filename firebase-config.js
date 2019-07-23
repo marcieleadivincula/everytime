57b43a0739800f75b9de90365aa0d99c57816aeb
nayamarosa <falecom@nayama.com.br>
2019-07-23 12:19:28 -0300

    [firebase][mocha]primeiro commit, configuração firebase e instal do mocha e chai

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1TMz-8cBm02hbx87Y9jqw0kMlIAw5OQU",
+    authDomain: "everytime-2cf38.firebaseapp.com",
+    databaseURL: "https://everytime-2cf38.firebaseio.com",
+    projectId: "everytime-2cf38",
+    storageBucket: "",
+    messagingSenderId: "402547150373",
+    appId: "1:402547150373:web:0d353e19859fd726"
+};
+
+firebase.initializeApp(firebaseConfig);
