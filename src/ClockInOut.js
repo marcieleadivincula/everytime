ee31a04cc5a2780534df3c821f890a6f226baf91
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-23 18:30:16 -0300

    [HUII-inOut]Teste check-in DOM

--- src/ClockInOut.js
+++ src/ClockInOut.js
@@ -1,14 +1,24 @@
+
 // const moment = require('moment');
 const moment = require("moment-business-time");
 
+function getMoment () {
+  const checkIn = moment();
+  console.log(checkIn)
+}
+
 const checkIn = moment();
+
+console.log('teste check-in' + document.getElementById('check-in').addEventListener('click', getMoment()));
+
 const date = new Date;
 const min = date.getMinutes();
 const checkOut = checkIn.subtractWorkingTime(7, 'hours', min, 'minutes');
 
-function getWorkedHours() {
-   console.log(checkOut);
+function getWorkedHours(checkIn, checkOut) {
+   // console.log(checkOut);
+   return checkOut - checkIn === 0 ? 'Horas ok' : 'Horas pend'
+
 }
 
-getWorkedHours();
-
+console.log(getWorkedHours(checkIn, checkOut));
