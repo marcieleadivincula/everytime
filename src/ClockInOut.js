947fba08f71c6fc96203519f64c049c853242867
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-23 15:18:10 -0300

    [HUII-inOut]implementa lógica inicial de workingDiff

--- src/ClockInOut.js
+++ src/ClockInOut.js
@@ -1,9 +1,15 @@
 // const moment = require('moment');
 const moment = require("moment-business-time");
 
-function ClockInOut() {
-   const now = moment();
-   console.log(now.subtractWorkingTime(2, 'hours'));
+const nowIn = '2015-02-27T16:30:00Z'
+const nowOut = '2015-02-26T12:00:00Z'
+
+function ClockInOut(nowIn, nowOut) {
+    const now = moment();
+
+    console.log(now)
+    console.log(now.subtractWorkingTime(6, 'hours'));
+    console.log(moment(nowIn).workingDiff(moment(nowOut), 'hours'));
 }
 
-ClockInOut();
+ClockInOut(nowIn, nowOut);
