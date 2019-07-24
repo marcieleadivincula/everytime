dba90d2b29bfc901ab623346b55adc93cecfc7b2
nayamarosa <falecom@nayama.com.br>
2019-07-24 11:50:27 -0300

    [js]adicionado clique do botão que traz o horário na tela

---
+++ src/js/check-hour.js
@@ -0,0 +1,19 @@
+document.querySelector(".check-in-style").addEventListener('click', () => {
+    const moment = functionName;
+    return document.querySelector(".check-in-hour").innerHTML = moment;
+})
+
+document.querySelector(".check-in-lunch-style").addEventListener('click', () => {
+    const moment = functionName;
+    return document.querySelector(".check-in-lunch-hour").innerHTML = moment;
+})
+
+document.querySelector(".check-out-lunch-style").addEventListener('click', () => {
+    const moment = functionName;
+    return document.querySelector(".check-out-lunch-hour").innerHTML = moment;
+})
+
+document.querySelector(".check-out-style").addEventListener('click', () => {
+    const moment = functionName;
+    return document.querySelector(".check-out-hour").innerHTML = moment;
+})
