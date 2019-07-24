f7b38197a27fd65ff1d14b17ca8df6184aa10871
JaquelineGDS <jaqueline_gd@outlook.com>
2019-07-24 12:58:48 -0300

    [js] select funcionarios funcionando

--- src/js/listEmployees.js
+++ src/js/listEmployees.js
@@ -1,18 +1,66 @@
-const database = firebase.firestore();
+const database = firebase.database();
+let listEmployees = document.querySelector(".listEmployees")
+const dataEmployee = document.querySelector(".dataEmployee")
+let valueSelect = "";
+
+listEmployees.addEventListener('change', function () {
+  valueSelect = listEmployees.value;
+  listEmployee(valueSelect)
+})
 
 window.onload = () => {
   getListEmployees();
 }
 
-function getListEmployees() {
-  let employees = [];
-
-  database.ref("users/").once('value')
+function listEmployee() {
+  console.log(valueSelect)
+  database.ref('/users/' + valueSelect).once('value')
     .then(function (snapshot) {
       snapshot.forEach(function (childSnapshot) {
         let childKey = childSnapshot.key;
         let childData = childSnapshot.val();
-        console.log(childKey, childData );
+        console.log(childKey, childData);
       });
+      // templateString();
     });
 }
+
+function getListEmployees() {
+  let employees = "";
+  database.ref('/users/').once('value')
+    .then(function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        let childKey = childSnapshot.key;
+        let childData = childSnapshot.val();
+        console.log(childKey, childData);
+        if (childData.profile === 'employee') {
+          employees += `<option value='${childKey}'>${childData.username}</option>`;
+        }
+      });
+      lististEmployees(employees)
+    });
+}
+
+function lististEmployees(employees) {
+  listEmployees.innerHTML = employees;
+}
+
+function addPostToDB(newtext, postType, like) {
+  console.log("addPostToDB")
+  return database.ref("schedules/" + uid ).push({
+    text: newtext,
+    postType: postType,
+    like: like
+  });
+}
+
+function templateString() {
+  const template = ` <div>
+  <ul>
+   <li>
+
+  </ul>
+  </div>`
+}
+
+
