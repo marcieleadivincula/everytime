64fff53b4789239f793c27a7daaf40cdf90da240
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-07-24 16:51:28 -0300

    atualiza branch

--- src/js/check-hour.js
+++ src/js/check-hour.js
@@ -1,19 +1,19 @@
 document.querySelector(".check-in-style").addEventListener('click', () => {
-    const moment = getDate();
+    const moment = checkIn();
     return document.querySelector(".check-in-hour").innerHTML = moment;
 })
 
 document.querySelector(".check-in-lunch-style").addEventListener('click', () => {
-    const moment = getDate();
+    const moment = checkOutForLunch();
     return document.querySelector(".check-in-lunch-hour").innerHTML = moment;
 })
 
 document.querySelector(".check-out-lunch-style").addEventListener('click', () => {
-    const moment = getDate();
+    const moment = checkInFromLunch();
     return document.querySelector(".check-out-lunch-hour").innerHTML = moment;
 })
 
 document.querySelector(".check-out-style").addEventListener('click', () => {
-    const moment = getDate();
+    const moment = checkOut();
     return document.querySelector(".check-out-hour").innerHTML = moment;
 })
