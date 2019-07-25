981d5808aafd4468b3a316fee00bd941abef88fb
BrunaGP <42470334+brubriele@users.noreply.github.com>
2019-07-25 13:45:38 -0300

    [test]
    Implementa estrura de testes atualizada.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,9 @@
 const chai = require("chai");
 const expect = chai.expect;
-const clockValidator = require("../src/index")
+const getWorkHours = require("../src/")
+
+describe('Carga horário cumprida sem banco de horas', () => {
+    it('CheckOut - CheckIn deve retornar 0', () => {
+        expect(getWorkHours(workLoad, checkIn, checkOut)).to.equal(true)
+    });
+})
